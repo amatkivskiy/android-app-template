7d49ae876fbd34eaa8e70af45708a2f933244ca1
Andriy Matkivskiy <andriy.matkivskiy@gmail.com>
2018-07-16 14:45:56 +0300

    Fix formatting issues of the model class.

--- app/src/main/java/com/github/amatkivskiy/template/domain/model/User.kt
+++ app/src/main/java/com/github/amatkivskiy/template/domain/model/User.kt
@@ -3,16 +3,16 @@ package com.github.amatkivskiy.template.domain.model
 import com.google.gson.annotations.SerializedName
 
 data class User(
-        @SerializedName("name") val name: Name,
-        @SerializedName("email") val email: String,
-        @SerializedName("picture") val picture: Picture,
-        @SerializedName("phone") val phone: String,
-        @SerializedName("cell") val cell: String
+    @SerializedName("name") val name: Name,
+    @SerializedName("email") val email: String,
+    @SerializedName("picture") val picture: Picture,
+    @SerializedName("phone") val phone: String,
+    @SerializedName("cell") val cell: String
 )
 
 data class Name(
-        @SerializedName("first") val first: String,
-        @SerializedName("last") val last: String
+    @SerializedName("first") val first: String,
+    @SerializedName("last") val last: String
 )
 
 data class Picture(@SerializedName("large") val large: String)
