e55665ee6c0d02c5dd32de71610d9fa564524c5a
Andriy Matkivskiy <andriy.matkivskiy@gmail.com>
2018-07-20 11:49:22 +0300

    Fix build script.

--- bitrise-ci-build.sh
+++ bitrise-ci-build.sh
@@ -2,9 +2,9 @@
 
 if [[ -z "${BITRISE_PULL_REQUEST}" ]]; then
     echo "---> Making build outside of Pull Request (building single commit or branch)"
-    ./gradlew clean gnagCheck build jacocoDebugReports
+    ./gradlew clean gnagCheck build jacocoDebugReport
 else
     echo "---> Making build for Pull Request"
     # In the env variable BITRISE_PULL_REQUEST Bitrise CI provides the ID of the PR
-    ./gradlew clean gnagReport -PauthToken="${GITHUB_TOKEN}" -PissueNumber="${BITRISE_PULL_REQUEST}" build jacocoDebugReports
+    ./gradlew clean gnagReport -PauthToken="${GITHUB_TOKEN}" -PissueNumber="${BITRISE_PULL_REQUEST}" build jacocoDebugReport
 fi
