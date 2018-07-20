a7b4721909c4dcf1b3dbfd1f5b823f64afacc14d
Andriy Matkivskiy <andriy.matkivskiy@gmail.com>
2018-07-20 11:25:01 +0300

    Add gnag support to the project

---
+++ bitrise-ci-build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [[ -z "${BITRISE_PULL_REQUEST}" ]]; then
+    echo "---> Making build outside of Pull Request (building single commit or branch)"
+    ./gradlew clean gnagCheck build jacocoDebugReports
+else
+    echo "---> Making build for Pull Request"
+    # In the env variable BITRISE_PULL_REQUEST Bitrise CI provides the ID of the PR
+    ./gradlew clean gnagReport -PauthToken="${GITHUB_TOKEN}" -PissueNumber="${BITRISE_PULL_REQUEST}" build jacocoDebugReports
+fi
