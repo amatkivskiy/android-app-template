0c1a678a3190b6d85a052be3ad22e0d7f6201fe6
Andriy Matkivskiy <andriy.matkivskiy@gmail.com>
2018-08-01 15:09:04 +0300

    Cover User data model constructor with test.

---
+++ app/src/test/java/com/github/amatkivskiy/template/model/UserTests.kt
@@ -0,0 +1,27 @@
+package com.github.amatkivskiy.template.model
+
+import com.github.amatkivskiy.template.domain.model.Name
+import com.github.amatkivskiy.template.domain.model.Picture
+import com.github.amatkivskiy.template.domain.model.User
+import org.amshove.kluent.`should be`
+import org.junit.Test
+
+class UserTests {
+
+    @Test
+    fun `user constructor works fine`() {
+        val name = Name("first", "second")
+        val email = "email"
+        val picture = Picture("image")
+        val phone = "phone"
+        val cell = "cell"
+
+        User(name, email, picture, phone, cell).let {
+            it.name `should be` name
+            it.email `should be` email
+            it.picture `should be` picture
+            it.phone `should be` phone
+            it.cell `should be` cell
+        }
+    }
+}
